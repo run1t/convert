e70acbc7cf94e269ece46af14c181ed1c28d18a2
run1t <reunanln@gmail.com>
2019-02-11 09:59:18 +0100

    :tada: Initial commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+export function convert<FROM, TO>(
+  src: FROM,
+  rules: {
+    copy?: Array<keyof FROM>;
+    rename?: { [k in keyof TO]?: keyof FROM };
+    map?: Array<{
+      dest: keyof TO;
+      map: (src: FROM) => any;
+    }>;
+  }
+): TO {
+  let obj: any = {};
+
+  if (rules.copy) {
+    obj = rules.copy.reduce(
+      (acc, property) => ({ ...acc, [property]: src[property] }),
+      obj
+    );
+  }
+
+  if (rules.rename) {
+    for (const property in rules.rename) {
+      if (rules.rename.hasOwnProperty(property)) {
+        obj = { ...obj, [property]: src[rules.rename[property]] };
+      }
+    }
+  }
+
+  if (rules.map) {
+    obj = rules.map.reduce(
+      (acc, property) => ({ ...acc, [property.dest]: property.map(src) }),
+      obj
+    );
+  }
+
+  return obj;
+}
